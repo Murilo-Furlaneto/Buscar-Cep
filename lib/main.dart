5817acad93c830dd5699099759629d966313b6cb
Beth <furlanetomurilo@gmail.com>
2022-01-31 00:45:22 -0300

    alterações no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 
 void main() {
   runApp(const MaterialApp(
@@ -19,32 +17,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   TextEditingController txtcep = TextEditingController();
-  late String resultado ='';
+  late String resultado = '';
 
-  _consultaCep() async{
+  _consultaCep() async {
     String cep = txtcep.text;
 
-    String url = 'https://viacep.com.br/ws/${cep}/json/';
+    String url = 'https://viacep.com.br/ws/$cep/json/';
 
     http.Response response;
     response = await http.get((Uri.parse(url)));
 
     Map<String, dynamic> retorno = json.decode(response.body);
- 
+
     String logradouro = retorno['logradouro'];
     String cidade = retorno['localidade'];
     String bairro = retorno['bairro'];
 
-
-    setState(() { //atualiza os valores da variavel resultado
+    setState(() {
+      //atualiza os valores da variavel resultado
       resultado = '$logradouro, $cidade,  $bairro';
-
     });
- 
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -56,35 +50,51 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        padding: const EdgeInsets.all(40),
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children:  <Widget>[
-             TextField(
-              keyboardType: TextInputType.number,
-              decoration: const  InputDecoration(
-                labelText: 'Digite o CEP ex: 1833400 ',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 200,
+              alignment: Alignment.topCenter,
+              child: Image.network(
+                'https://store-images.s-microsoft.com/image/apps.6607.13510798887520085.3b5999bd-0689-4a5d-b1fa-378e87bb83a5.ee076621-7430-46f1-a4ac-a0c442d69e58',
+                fit: BoxFit.contain,
               ),
-              style: const TextStyle(fontSize: 20),
-              controller: txtcep,
             ),
-            Text('Resultado: $resultado',
-            style: const TextStyle(fontSize: 20)),
-             
-
-            ElevatedButton(
-            onPressed: _consultaCep, 
-            child: const Text('Consultar',
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.white,
-            )),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blue)),
+            Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Digite o CEP ex: 1833400 ',
+                ),
+                style: const TextStyle(fontSize: 20),
+                controller: txtcep,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 26),
+              child: Center(
+                  child: Text('Resultado: $resultado',
+                      style: const TextStyle(fontSize: 20))),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: ElevatedButton(
+                  onPressed: _consultaCep,
+                  child: const Text('Consultar',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      )),
+                  style: ElevatedButton.styleFrom(primary: Colors.blue)),
+            ),
           ],
         ),
       ),
-
     );
   }
 }
